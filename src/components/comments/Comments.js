2861521cecd4b2f7146f6146163cce02d82fb3dd
Hensetsu <nothile1@gmail.com>
2022-08-03 18:44:57 +0100

    Added ability to add comments

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -7,8 +7,13 @@ import { DUMMY_COMMENTS } from '../../data/dummy_comments';
 import CommentsList from './CommentsList';
 
 const Comments = (props) => {
+
+  //session storage data for comments
+  sessionStorage.getItem('comments') === null && sessionStorage.setItem('comments', JSON.stringify(DUMMY_COMMENTS.filter(index => index.quoteId === props.quoteId))); 
+
   const [isAddingComment, setIsAddingComment] = useState(false);
   const [showComments, setShowComments] = useState(false);
+  const [comments, setComments] = useState(DUMMY_COMMENTS.filter(index => index.quoteId === props.quoteId));
 
   const showCommentHandler = (event) => {
     event.preventDefault();
@@ -18,20 +23,26 @@ const Comments = (props) => {
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
-
-  const comments = DUMMY_COMMENTS.filter(index => index.quoteId === props.quoteId);
   
   return (
     <section className="comments">
-      <h2>User Comments</h2>
-      {!isAddingComment && (
-        <button className='btn' onClick={startAddCommentHandler}>
-          Add a Comment
-        </button>
-      )}
-      {isAddingComment && <NewCommentForm />}
+
       <button className="btn btn--flat" onClick={showCommentHandler}>Load comments</button>
-      {showComments && <CommentsList comments={comments}/>}
+
+      {
+        showComments && (
+          <div>
+            <h2>User Comments</h2>
+            {!isAddingComment && (
+              <button className='btn' onClick={startAddCommentHandler}>
+                Add a Comment
+              </button>
+            )}
+            {isAddingComment && <NewCommentForm setIsAdding={setIsAddingComment} setComments={setComments} quoteId={props.quoteId}/>}
+            {comments.length > 0 ? <CommentsList comments={comments}/> : <b>No comments were added yet!</b>}
+          </div>
+        )
+      }
     </section>
   );
 };
