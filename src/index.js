7d47c203faec77e0e98c1bdfa222160fed46be6f
Hensetsu <nothile1@gmail.com>
2022-08-07 18:05:18 +0100

    Swpped to hashrouter so I can reload the page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './index.scss';
 import App from './App';
 
